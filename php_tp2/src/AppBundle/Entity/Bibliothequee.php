6ce795c2df85ac91c1bde5b7c120e1c25e02765c
Ozcan SEVIK <sevik.ozcan@gmail.com>
2018-01-18 16:09:10 +0100

    Entity Bibliotheque generation

---
+++ php_tp2/src/AppBundle/Entity/Bibliothequee.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Bibliothequee
+ */
+class Bibliothequee
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $adresse;
+
+    /**
+     * @var string
+     */
+    private $director;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Bibliothequee
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set author
+     *
+     * @param string $author
+     * @return Bibliothequee
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return string 
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set adresse
+     *
+     * @param string $adresse
+     * @return Bibliothequee
+     */
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    /**
+     * Get adresse
+     *
+     * @return string 
+     */
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+
+    /**
+     * Set director
+     *
+     * @param string $director
+     * @return Bibliothequee
+     */
+    public function setDirector($director)
+    {
+        $this->director = $director;
+
+        return $this;
+    }
+
+    /**
+     * Get director
+     *
+     * @return string 
+     */
+    public function getDirector()
+    {
+        return $this->director;
+    }
+}
